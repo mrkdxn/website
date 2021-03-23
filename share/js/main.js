8540a6dd0f2f7bf1efa88d037ad3b318a705f9e9
Mark Dixon <mark.dixon@lightspeedhq.com>
2021-03-23 17:53:12 -0400

    v2

--- share/js/main.js
+++ share/js/main.js
@@ -3,9 +3,29 @@ console.log('test');
 let config =
     [
         {
-            "title" : "Winter 2019/20",
+            "title" : "Spring 2021",
+            "img" : "https:////live.staticflickr.com/65535/51060919222_3183dabb97_n.jpg",
+            "link" : "https://www.flickr.com/gp/80995589@N00/8u5trR"
+        },
+        {
+            "title" : "Winter 2021",
+            "img" : "https://live.staticflickr.com/65535/51033879281_02e7eae455_n.jpg",
+            "link" : "https://www.flickr.com/gp/80995589@N00/FyR119"
+        },
+        {
+            "title" : "Winter 2020",
             "img" : "https://live.staticflickr.com/65535/49336778773_ea618b9ae2_h.jpg",
             "link" : "https://www.flickr.com/gp/80995589@N00/0MP7T3"
+        },
+        {
+            "title" : "Fall 2020",
+            "img" : "https://live.staticflickr.com/65535/50538152618_d75e15dd53_n.jpg",
+            "link" : "https://www.flickr.com/gp/80995589@N00/LZ898u"
+        },
+        {
+            "title" : "Spring/Summer 2020",
+            "img" : "https://live.staticflickr.com/65535/50230972657_56e0062ae3_n.jpg",
+            "link" : "https://www.flickr.com/gp/80995589@N00/B7953r"
         },
         {
             "title" : "Spring to Fall 2019",
@@ -42,11 +62,14 @@ for (var key in config) {
             photoClass = ' featured';
         }
         let addPhoto = '';
+
+        addPhoto += '<div class="photo ' + photoClass + '">';
         addPhoto += '<a href="' + config[key].link + '">';
-        addPhoto += '<div class="photo ' + photoClass + '" style="background: url('+ config[key].img + ')">';
+        addPhoto += '<img src="' + config[key].img + '"/>';
         addPhoto += '<h2>' + config[key].title + '</h2>';
-        addPhoto += '</div>';
         addPhoto += '</a>';
+        addPhoto += '</div>';
+
         photos.innerHTML += addPhoto;
     }
 }
