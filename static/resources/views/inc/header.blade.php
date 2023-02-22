f5b299575a497a21451f76c85d7e0d4494f5d9fa
Mark Dixon <mrkdxn@users.noreply.github.com>
2023-02-22 17:31:46 -0500

    GTM ID

--- static/resources/views/inc/header.blade.php
+++ static/resources/views/inc/header.blade.php
@@ -38,13 +38,13 @@
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-5ZC34FH');</script>
+    })(window,document,'script','dataLayer','GTM-WQ6TWR3');</script>
     <!-- End Google Tag Manager -->
 
 </head>
 <body>
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5ZC34FH"
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WQ6TWR3"
                   height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
 <div id="header">
