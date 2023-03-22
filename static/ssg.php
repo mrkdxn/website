672561fae07677d120f5d3c6b9219695964e4ca1
Mark Dixon <mrkdxn@users.noreply.github.com>
2023-03-22 17:17:53 -0400

    SSG fixes & timestamp

--- static/ssg.php
+++ static/ssg.php
@@ -10,32 +10,41 @@ class SSG
 	
 	public function init()
 	{
-		echo '<h1>Building static site...</h1>';
+		date_default_timezone_set("America/New_York");
+
+		echo '========================================'. PHP_EOL;
+		echo 'Building static site...'. PHP_EOL;
 		$contentDir = __DIR__ . '/content';
 		$contentFiles = array_diff(scandir($contentDir), array('.', '..'));
 		
 		foreach ($contentFiles as $file) {
-			$data = json_decode(file_get_contents($contentDir . '/' . $file));
+			//var_dump($file);
+			if ($file != '.DS_Store')
+			{
+				$data = json_decode(file_get_contents($contentDir . '/' . $file));
+				$blade = new Blade(__DIR__ . '/resources/views', __DIR__ . '/cache');
 
-			$blade = new Blade(__DIR__ . '/resources/views', __DIR__ . '/cache');
-			$theView = $data->view ;
-			$content = $blade->view()
-			->make($theView, get_object_vars($data))
-			->render();
+				$theView = $data->view ;
+				$content = $blade->view()
+				->make($theView, get_object_vars($data))
+				->render();
 
-			$outputDir = dirname(__DIR__, 1) . '/docs' . $data->slug;
-			if(!is_dir($outputDir))
-				{
-					mkdir($outputDir, 0755, true);
-				}
-			$outputPath = $outputDir . '/index.html';
-			file_put_contents($outputPath, $content);
+				$outputDir = dirname(__DIR__, 1) . '/docs' . $data->slug;
+				if(!is_dir($outputDir))
+					{
+						mkdir($outputDir, 0755, true);
+					}
+				$outputPath = $outputDir . '/index.html';
+				file_put_contents($outputPath, $content);
 
-			echo 'Built page: ' . $outputPath . '<br><br>';
+				echo 'Built page: ' . $outputPath . PHP_EOL;
+			}
+			
 		}
-		echo "<strong>Done.</strong>";
+		echo date("h:i:sa") . PHP_EOL;
+		echo 'DONE.'. PHP_EOL;
+		echo '========================================'. PHP_EOL;
 	}
-
 
 }
 
