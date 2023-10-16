5d4f35d79fa4929bc806fe711a6e18c5404c01b0
Mark Dixon <mrkdxn@users.noreply.github.com>
2023-10-16 16:53:33 -0400

    Update insta.js

--- docs/js/insta.js
+++ docs/js/insta.js
@@ -1,8 +1,7 @@
 // get data from https://mrkdxn.infinityfreeapp.com/insta.php?p=poisson and console.log it
-// create a function to get the data from the url
 
 function getData(url) {
-    return fetch(`https://cors-anywhere.herokuapp.com/${url}`)
+    return fetch(url)
         .then((resp) => resp.json())
         .then(function(data) {
             console.log(data);
