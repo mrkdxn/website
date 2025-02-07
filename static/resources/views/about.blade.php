1a7d65a8b63978b38bf878cf5ec3d22b1f8fd564
Mark Dixon <mrkdxn@users.noreply.github.com>
2025-02-07 15:08:37 -0500

    Add back insta fetch

--- static/resources/views/about.blade.php
+++ static/resources/views/about.blade.php
@@ -1,8 +1,17 @@
 @include('inc/header')
 
-about
-	<h1>{{ $h1 }}</h1>
+	<h2>{{ $h1 }}</h2>
 
-345
+	<p>This is not an artist’s statement.</p>
+
+	<p>After many years focusing on other things I’ve started to make art again. A slow return that feels like it makes much sense and is much needed. Ideas of paintings started haunting me as I lay in bed, trying to sleep.</p>
+
+	<p>I no longer have my large studio where I can make large canvases and large messes. I also no longer have large swaths of time to spend working there. So I have begun to make small sketches, tests, experiments, errors, to get back into the groove of things. Muscle memory. </p>
+
+	<p>In the past my artworks have oscillated from landscapes to abstraction. I’ve been hesitant to carve a path but recently my drawings and paintings have leaned towards the landscape/forest. I spend a lot of time there - in mind and in person.</p>
+
+	<p>I currently live in Montreal and am originally from New Brunswick. I have a full-time day job that is far from the shore of art making. I am originally from New Brunswick and sometimes visit there in mind and in person.  </p>
+
+	<p>I studied at the Nova Scotia College of Art & Design [BFA] and Concordia University [MFA] with a brief stint at Rhode Island School of Design [Foundation Year]. I have exhibited at galleries in eastern Canada, received a few art grants [NB Arts Branch] and sold works to private collections and The New Brunswick Art Bank.</p>
 
 @include('inc/footer')
